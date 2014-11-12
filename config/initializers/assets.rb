c1cd3b436288fd6427ac6c44e873a1f159ffbcd2
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-11-12 17:44:00 +0000

    Add govuk elements as a bower component.
    NB: Pinned down to latest commit

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,4 @@
 Rails.application.config.assets.version = '1.0'
 
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components',
+                                                         'govuk_elements', 'public', 'sass')
