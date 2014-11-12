49f4b7519c7abe4d303da0dd2c821ce5a2319fcb
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-11-12 15:05:00 +0000

    Use the govuk template as the main layout.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 ruby '2.1.4'
 
 gem 'rails', '4.1.7'
+gem 'govuk_template'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout 'govuk_template'
 end
