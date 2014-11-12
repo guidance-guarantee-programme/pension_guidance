8f888d592ef6779bd5010cb1c8aa8575c2e87077
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-11-12 17:45:22 +0000

    Example article index page.
    @aduggin :musical_note:

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,5 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components',
                                                          'govuk_elements', 'public', 'sass')
+
+Rails.application.config.assets.precompile += %w( pensions-guidance.css )
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  # root 'welcome#index'
+  root 'articles#index'
 end
