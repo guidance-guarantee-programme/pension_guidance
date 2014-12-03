c1dc7d5f0bea3ddcb43ea18579a847bd2aef8db7
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-12-03 13:18:57 +0000

    Add basic auth when in production.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD'] if Rails.env.production?
 
   layout 'govuk_template'
 end
