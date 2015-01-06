70d9d29b580a9955b9c1ad107c5528112350a0a3
Andrew Garner <andrew@andrewgarner.com>
2015-01-06 14:50:08 +0000

    Upgrade to Rails 4.2.0

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
 
+  # Randomize the order test cases are executed.
+  config.active_support.test_order = :random
+
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
