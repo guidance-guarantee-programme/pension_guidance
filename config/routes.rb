7bede07ae178b44daab5152e418e1f240f574184
Andrew Garner <andrew@andrewgarner.com>
2015-01-21 10:43:11 +0000

    Only accept requests for HTML resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show]
-
-  get 'styleguide/pages/article', controller: 'styleguide#pages_article'
-  get 'styleguide/pages/homepage', controller: 'styleguide#pages_homepage'
-
-  get 'styleguide(/:action)', controller: 'styleguide'
-
   root 'home#show'
+
+  constraints format: 'html' do
+    resources :articles, only: [:show]
+
+    get 'styleguide/pages/article', controller: 'styleguide#pages_article'
+    get 'styleguide/pages/homepage', controller: 'styleguide#pages_homepage'
+
+    get 'styleguide(/:action)', controller: 'styleguide'
+  end
 end
