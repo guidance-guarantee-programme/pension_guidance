5dbac8da173f79448f901c7244d539fe481c4dc2
Andrew Garner <andrew@andrewgarner.com>
2015-01-21 11:08:59 +0000

    Compress responses
    Further reading: http://robots.thoughtbot.com/content-compression-with-rack-deflater

---
+++ spec/requests/compression_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe 'Compression', type: :request do
+  ['deflate', 'gzip', 'deflate,gzip', 'gzip,deflate'].each do |compression_method|
+    context "a visitor has a browser that supports '#{compression_method}' compression" do
+      it 'compresses the content' do
+        get '/articles/know-your-pension-type', {}, 'HTTP_ACCEPT_ENCODING' => compression_method
+
+        expect(response.headers['Content-Encoding']).to be
+      end
+    end
+  end
+
+  context "a visitor's browser does not support compression" do
+    it 'does not compresses the content' do
+      get '/articles/know-your-pension-type'
+
+      expect(response.headers['Content-Encoding']).to_not be
+    end
+  end
+end
