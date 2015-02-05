a0465503e834303327fac812699c4b1f974cd44e
Andrew Garner <andrew@andrewgarner.com>
2015-02-05 11:55:15 +0000

    Don’t expose Journey’s steps as it is enumerable

--- app/models/journey.rb
+++ app/models/journey.rb
@@ -1,14 +1,11 @@
 class Journey
   include Enumerable
 
-  attr_accessor :steps
-  private :steps=
-
   def initialize(*steps)
-    self.steps = steps
+    @steps = steps
   end
 
   def each(&block)
-    steps.each(&block)
+    @steps.each(&block)
   end
 end
