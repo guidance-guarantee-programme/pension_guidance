ee00582776d3787fc6ae203f0c5b22358c0e90bd
Steven Wilkin <stevenwilkin@gmail.com>
2015-02-09 14:20:34 +0000

    Refactor style guide controller
    Don't need to repeat the layout name in the calls to `render`

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -2,18 +2,18 @@ class StyleguideController < ApplicationController
   layout 'styleguide/full_width'
 
   def pages_homepage
-    render layout: 'styleguide/full_width', template: 'styleguide/pages/homepage'
+    render template: 'styleguide/pages/homepage'
   end
 
   def pages_guide
-    render layout: 'styleguide/full_width', template: 'styleguide/pages/guide'
+    render template: 'styleguide/pages/guide'
   end
 
   def pages_journey_index
-    render layout: 'styleguide/full_width', template: 'styleguide/pages/journey_index'
+    render template: 'styleguide/pages/journey_index'
   end
 
   def pages_journey_page
-    render layout: 'styleguide/full_width', template: 'styleguide/pages/journey_page'
+    render template: 'styleguide/pages/journey_page'
   end
 end
