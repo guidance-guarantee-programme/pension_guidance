5afc6fcc5b4c8a73148b650fe87c4110eb6333cf
Gareth Visagie <gareth@gjvis.com>
2015-06-02 14:13:07 +0100

    Remove unnecessary comments in click-tracker

--- app/assets/javascripts/click-tracker.js
+++ app/assets/javascripts/click-tracker.js
@@ -14,25 +14,25 @@
       };
 
       // global
-      track('#global-header a', 'global header'); // global header links
-      track('.l-masthead a', 'masthead'); // masthead links
-      track('#footer a', 'footer'); // footer links
+      track('#global-header a', 'global header');
+      track('.l-masthead a', 'masthead');
+      track('#footer a', 'footer');
 
       // homepage
-      track('.quick-links a', 'quick links'); // quick links
-      track('.l-home-top-slot .l-column-third a', 'introduction'); // introduction links
-      track('.l-home-top-slot-promo a', 'appointments'); // appointment links
-      track('.journey-promo a', 'journey promo'); // journey promo links
-      track('.l-home-guides a', 'guides directory'); // guide directory links
-      track('.l-home-about a', 'about the service'); // about service links
+      track('.quick-links a', 'quick links');
+      track('.l-home-top-slot .l-column-third a', 'introduction');
+      track('.l-home-top-slot-promo a', 'appointments');
+      track('.journey-promo a', 'journey promo');
+      track('.l-home-guides a', 'guides directory');
+      track('.l-home-about a', 'about the service');
 
       //guides
-      track('.breadcrumbs a', 'breadcrumb'); // breadcrumb links
-      track('article a', 'content'); // content links
-      track('.ga-options-table a', 'options table'); // table links
-      track('.pager a', 'pager'); // pager links
-      track('.ga-journey-sidebar a', 'journey sidebar'); // journey sidebar links
-      track('.ga-elsewhere-sidebar a', 'elsewhere sidebar'); // journey elsewhere links
+      track('.breadcrumbs a', 'breadcrumb');
+      track('article a', 'content');
+      track('.ga-options-table a', 'options table');
+      track('.pager a', 'pager');
+      track('.ga-journey-sidebar a', 'journey sidebar');
+      track('.ga-elsewhere-sidebar a', 'elsewhere sidebar');
     }
   };
 
