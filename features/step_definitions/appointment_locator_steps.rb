561bc10d38139d610fda049164e5ae2bc817ce14
Steven Wilkin <stevenwilkin@gmail.com>
2015-06-10 14:43:20 +0100

    Push faking down from `Locations.nearest_to_postcode` into it's collaborators

--- features/step_definitions/appointment_locator_steps.rb
+++ features/step_definitions/appointment_locator_steps.rb
@@ -1,9 +1,13 @@
 When(/^I search for appointment locations near to a valid postcode$/) do
+  locations = 1.upto(7).map { |x| Locations::Location.new("cab-#{x}") }
   postcode = 'SW1A 2HQ'
+  lat_lng = [1, 0]
+  limit = 5
 
-  allow(Locations).to receive(:nearest_to_postcode).with(postcode, limit: 5) do
-    1.upto(5).map { |x| Locations::Location.new("cab-#{x}") }
-  end
+  allow(Locations::Repository).to receive(:all).and_return(locations)
+  allow(Geocoder).to receive(:coordinates).with(postcode).and_return(lat_lng)
+  allow(Locations::Search).to receive(:nearest_to).with(
+    locations, lat_lng, limit).and_return(locations.take(limit))
 
   Pages::Locations.new.load(postcode: postcode)
 end
--- lib/locations.rb
+++ lib/locations.rb
@@ -1,5 +1,9 @@
 module Locations
-  def self.nearest_to_postcode(_postcode, limit:)
-    [].take(limit)
+  def self.nearest_to_postcode(postcode,
+      geocoder: Geocoder, repository: Repository, search: Search, limit:)
+    lat_lng = geocoder.coordinates(postcode)
+    locations = repository.all
+
+    search.nearest_to(locations, lat_lng, limit)
   end
 end
