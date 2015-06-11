57000b387ccac63c9fc7c34a8912e262bd469b8c
Steven Wilkin <stevenwilkin@gmail.com>
2015-06-11 12:51:45 +0100

    Sort locations by distance from given lat_lng

--- lib/locations/search.rb
+++ lib/locations/search.rb
@@ -1,7 +1,13 @@
 module Locations
   class Search
-    def self.nearest_to(locations, _lat_lng, limit)
-      locations.take(limit)
+    def self.nearest_to(locations, lat_lng, limit)
+      search_results = locations.map do |location|
+        distance = Geocoder::Calculations.distance_between(
+          location.lat_lng, lat_lng, units: :mi)
+        SearchResult.new(location, distance)
+      end
+
+      search_results.sort_by(&:distance).take(limit)
     end
   end
 end
---
+++ spec/lib/locations/search_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe Locations::Search, '.nearest_to' do
+  let(:london) { Locations::Location.new('London', [51.500152, -0.126236]) }
+  let(:paris) { Locations::Location.new('Paris', [48.85666, 2.350987]) }
+  let(:new_york) { Locations::Location.new('New York', [40.714269, 74.005973]) }
+  let(:belfast) { [54.597269, -5.930109] }
+  let(:locations) { [new_york, paris, london] }
+  let(:limit) { 5 }
+
+  subject(:results) { described_class.nearest_to(locations, belfast, limit) }
+
+  it { is_expected.to eq([london, paris, new_york]) }
+
+  context 'when limiting results' do
+    let(:limit) { 2 }
+
+    subject { results.count }
+
+    it { is_expected.to eq(limit) }
+  end
+end
