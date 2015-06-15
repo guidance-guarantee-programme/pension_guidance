6693f971059c8470aa2d4b466cc2a8ebd4e1a34a
Steven Wilkin <stevenwilkin@gmail.com>
2015-06-15 13:59:37 +0100

    Present search results in line with mockups

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,5 +1,15 @@
 class LocationsController < ApplicationController
+  layout 'full_width'
+
   def index
-    @locations = Locations.nearest_to_postcode(params[:postcode], limit: 5)
+    @locations = locations.map do |location|
+      SearchResultDecorator.new(location)
+    end
+  end
+
+  private
+
+  def locations
+    Locations.nearest_to_postcode(params[:postcode], limit: 5)
   end
 end
