ba352998843a35ed8017f0dbc83b7d615966a323
Steven Wilkin <stevenwilkin@gmail.com>
2015-06-16 13:07:32 +0100

    `RGeo` is now a dependancy so remove use of `Geocoder`

--- lib/locations/search.rb
+++ lib/locations/search.rb
@@ -1,13 +1,21 @@
 module Locations
   class Search
+    METRES_PER_MILE = 1609.344
+
     def self.nearest_to(locations, lat_lng, limit)
+      start_point = point(lat_lng)
+
       search_results = locations.map do |location|
-        distance = Geocoder::Calculations.distance_between(
-          location.lat_lng, lat_lng, units: :mi)
+        end_point = point(location.lat_lng)
+        distance = start_point.distance(end_point) / METRES_PER_MILE
         SearchResult.new(location, distance)
       end
 
       search_results.sort_by(&:distance).take(limit)
+    end
+
+    def self.point(lat_lng)
+      RGeo::Geographic.spherical_factory.point(lat_lng[1], lat_lng[0])
     end
   end
 end
