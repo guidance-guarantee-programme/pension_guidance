4c8371c67095d3b5fd3ff0bf255c871eae5656d2
Steven Wilkin <stevenwilkin@gmail.com>
2015-06-19 11:57:52 +0100

    Raise exceptions on invalid postcodes and failed lookups

--- lib/geocoder.rb
+++ lib/geocoder.rb
@@ -1,7 +1,14 @@
 class Geocoder
-  def self.lookup(postcode)
-    lookup = Postcodes::IO.new.lookup(postcode)
+  InvalidPostcode = Class.new(StandardError)
+  InvalidLookup = Class.new(StandardError)
 
-    [lookup.latitude, lookup.longitude] if lookup
+  def self.lookup(postcode)
+    ukp = UKPostcode.parse(postcode)
+    fail InvalidPostcode unless ukp.full_valid?
+
+    lookup = Postcodes::IO.new.lookup(postcode)
+    fail InvalidLookup unless lookup
+
+    [lookup.latitude, lookup.longitude]
   end
 end
