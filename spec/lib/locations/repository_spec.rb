582db7744d7004d5e4e0838818cdccafb9b2dcde
Steven Wilkin <stevenwilkin@gmail.com>
2015-06-23 15:47:27 +0100

    Make Locations repository searchable

--- spec/lib/locations/repository_spec.rb
+++ spec/lib/locations/repository_spec.rb
@@ -5,12 +5,30 @@ RSpec.describe Locations::Repository do
   let(:phone) { '0300 1 233 233' }
   let(:lat_lng) { [54.5957306, -5.9341485] }
   let(:location) { locations.first }
+  let(:repository) { described_class.new(path) }
 
-  subject(:locations) { described_class.new(path).all }
+  subject(:locations) { repository.all }
 
   specify { expect(locations.count).to eq(1) }
   specify { expect(location.name).to eq(name) }
   specify { expect(location.address).to eq(address) }
   specify { expect(location.phone).to eq(phone) }
   specify { expect(location.lat_lng).to eq(lat_lng) }
+
+  describe '#find' do
+    let(:id) { 'invalid' }
+
+    subject(:result) { repository.find(id) }
+
+    context 'with an invalid id' do
+      it { is_expected.to be_nil }
+    end
+
+    context 'with an valid id' do
+      let(:id) { 'belfast-citizens-advice-bureau' }
+
+      specify { expect(result).to be_a(Locations::Location) }
+      specify { expect(result.name).to eq(name) }
+    end
+  end
 end
