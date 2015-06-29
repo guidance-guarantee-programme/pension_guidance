631471cd1ad3acbef861cc725a53cb3a68589261
Andrew Garner <andrew@andrewgarner.com>
2015-06-29 19:04:00 +0100

    Use the GeoJSON Feature ID to identify locations
    Generating a slug for each location based on its title is too brittle.
    We know location titles will change. If that happens previously
    addressable locations will no longer be addressable.
    
    The GeoJSON specification supports an ID attribute for each location. By
    using that we can be continue to address a location if its title
    changes.

--- spec/lib/locations/location_spec.rb
+++
@@ -1,7 +0,0 @@
-RSpec.describe Locations::Location do
-  let(:name) { 'Local & Beyond - CAB' }
-
-  subject(:location) { described_class.new(name, 'address', 'phone', 'hours', [1, 0]) }
-
-  specify { expect(location.id).to eq('local-beyond-cab') }
-end
