67be3e6bf036115e714bd8cb6d80a9631f717f20
Steven Wilkin <stevenwilkin@gmail.com>
2015-06-30 15:32:38 +0100

    Create a reader which will cache externally retrieved JSON

---
+++ lib/locations/reader.rb
@@ -0,0 +1,39 @@
+require 'open-uri'
+
+module Locations
+  class Reader
+    attr_accessor :path, :redis
+
+    def initialize(path, redis = Redis.new)
+      self.path = path
+      self.redis = redis
+    end
+
+    def call
+      if json_from_url?
+        cached_json
+      else
+        read_json(path)
+      end
+    end
+
+    private
+
+    def json_from_url?
+      path.to_s =~ /^http/
+    end
+
+    def cached_json
+      json = redis.get('locations')
+      unless json
+        json = read_json(path)
+        redis.setex('locations', 60 * 60, json)
+      end
+      json
+    end
+
+    def read_json(path)
+      open(path).read
+    end
+  end
+end
---
+++ spec/lib/locations/reader_spec.rb
@@ -0,0 +1,72 @@
+require 'fakeredis'
+
+RSpec.describe Locations::Reader, '#call' do
+  let(:now) { Time.zone.local(2015, 6, 30, 14) }
+  let(:key) { 'locations' }
+  let(:json) { 'json' }
+  let(:path) { '/path/to/locations.json' }
+  let(:redis) { Redis.new }
+  let(:reader) { described_class.new(path, redis) }
+
+  def json_in_redis
+    redis.get(key)
+  end
+
+  def keys_in_redis
+    redis.keys('*')
+  end
+
+  def expiry_in_seconds
+    redis.ttl(key)
+  end
+
+  subject(:read) { reader.call }
+
+  before do
+    Timecop.freeze(now)
+    redis.flushdb
+    allow(reader).to receive(:read_json).with(path).and_return(json)
+  end
+
+  after do
+    Timecop.return
+  end
+
+  context 'with a path on the filesystem' do
+    before do
+      read
+    end
+
+    it { is_expected.to eq(json) }
+
+    specify { expect(keys_in_redis).to be_empty }
+  end
+
+  context 'with a url' do
+    let(:path) { 'http://locator/locations.json' }
+
+    context 'and the data does not exist in Redis' do
+      before do
+        read
+      end
+
+      it { is_expected.to eq(json) }
+
+      specify { expect(json_in_redis).to eq(json) }
+      specify { expect(expiry_in_seconds).to eq(60 * 60) }
+    end
+
+    context 'and the data exists in Redis' do
+      let(:existing_json) { 'existing json' }
+
+      before do
+        redis.set(key, existing_json)
+        read
+      end
+
+      it { is_expected.to eq(existing_json) }
+
+      specify { expect(json_in_redis).to eq(existing_json) }
+    end
+  end
+end
