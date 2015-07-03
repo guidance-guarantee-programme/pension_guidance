8c2c26601d4f17ef32299178fa64c59f9a7bf7e9
Andrew Garner <andrew@andrewgarner.com>
2015-07-03 16:02:23 +0100

    Return cache headers for location pages

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,6 +2,8 @@ class LocationsController < ApplicationController
   layout 'locations'
 
   def index
+    expires_in Rails.application.config.cache_max_age, public: true
+
     return render :search unless params[:postcode].present?
 
     @postcode = params[:postcode]
@@ -17,6 +19,8 @@ class LocationsController < ApplicationController
     @location = Locations.find(params[:id])
 
     fail(ActionController::RoutingError, 'Location Not Found') unless @location
+
+    expires_in Rails.application.config.cache_max_age, public: true
   end
 
   private
--- config/initializers/locations.rb
+++ config/initializers/locations.rb
@@ -1,1 +1,5 @@
-Locations.geo_json_path_or_url = ENV['LOCATIONS_URL'] || 'https://locator.pensionwise.gov.uk/locations.json'
+Locations.geo_json_path_or_url = if Rails.env.test?
+                                   Rails.root.join('features', 'fixtures', 'locations.json')
+                                 else
+                                   ENV['LOCATIONS_URL'] || 'https://locator.pensionwise.gov.uk/locations.json'
+                                 end
