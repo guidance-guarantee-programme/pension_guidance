08e488db5b9226ef88eb8cdc842cdfcff5a2c39d
Alistair Duggin <alistair.duggin@btinternet.com>
2015-08-04 11:30:27 +0100

    Add breadcrumbs to style guide locations pages

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -18,22 +18,27 @@ class StyleguideController < ApplicationController
   end
 
   def pages_locations_search
-    render template: 'styleguide/pages/locations_search'
+    render template: 'styleguide/pages/locations_search',
+           layout: 'styleguide/pages'
   end
 
   def pages_locations_index
-    render template: 'styleguide/pages/locations_index'
+    render template: 'styleguide/pages/locations_index',
+           layout: 'styleguide/pages'
   end
 
   def pages_locations_invalid
-    render template: 'styleguide/pages/locations_invalid'
+    render template: 'styleguide/pages/locations_invalid',
+           layout: 'styleguide/pages'
   end
 
   def pages_locations_show
-    render template: 'styleguide/pages/locations_show'
+    render template: 'styleguide/pages/locations_show',
+           layout: 'styleguide/pages'
   end
 
   def pages_locations_show_call_centre
-    render template: 'styleguide/pages/locations_show_call_centre'
+    render template: 'styleguide/pages/locations_show_call_centre',
+           layout: 'styleguide/pages'
   end
 end
