1dcec46fab5c628c81b203f80d2a31cbc5e160d9
Steven Wilkin <stevenwilkin@gmail.com>
2015-08-06 11:30:15 +0100

    Don't set the layout in `ApplicationController`
    Every controller inheriting from it set's it own layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,4 @@ class ApplicationController < ActionController::Base
   if ENV['AUTH_USERNAME'] && ENV['AUTH_PASSWORD']
     http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
   end
-
-  layout 'govuk_template'
 end
