eb1a700e5a9ca1a0690b29bc04456b89b8e03fcc
Steven Wilkin <stevenwilkin@gmail.com>
2015-09-01 15:55:07 +0100

    Use Faraday rather than Excon for Switchboard lookups
    We're using Faraday for Google Custom Search so be consistent

--- lib/switchboard.rb
+++ lib/switchboard.rb
@@ -3,22 +3,20 @@ class Switchboard
     def lookup(id)
       return unless base_url
 
-      response = Excon.new("#{base_url}/lookup/#{id}").get
-      return unless response.status == 200
-
-      begin
-        json = JSON.parse(response.body)
-      rescue JSON::ParserError
-        return
-      end
-
-      json['phone']
+      response = connection.get("/lookup/#{id}")
+      response.body['phone']
+    rescue HTTPConnection::ResourceNotFound, HTTPConnection::ClientError
+      return
     end
 
     private
 
     def base_url
       ENV['SWITCHBOARD_BASE_URL']
+    end
+
+    def connection
+      HTTPConnectionFactory.build(base_url)
     end
   end
 end
