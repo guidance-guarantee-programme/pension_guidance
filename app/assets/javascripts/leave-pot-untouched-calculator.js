04476bc0c3372870142a7c2ffcec6d390e54e77c
Steven Wilkin <stevenwilkin@gmail.com>
2015-09-30 16:06:07 +0100

    Make the "Re-calculate" button submit the form

--- app/assets/javascripts/leave-pot-untouched-calculator.js
+++ app/assets/javascripts/leave-pot-untouched-calculator.js
@@ -3,11 +3,11 @@
 
   var leavePotUntouchedCalculator = {
     init: function() {
-      var $calculator = $('.js-leave-pot-untouched-calculator');
+      var $calculator = $('.js-leave-pot-untouched-calculator form');
 
       $calculator.append('<div class="calculator__result" aria-live="polite"></div>');
 
-      $calculator.find('form').on('submit', function(event) {
+      $calculator.on('submit', function(event) {
         event.preventDefault();
         $.get('/leave-pot-untouched/results', $(this).serialize(), function(data) {
           $('.calculator__result').html(data);
