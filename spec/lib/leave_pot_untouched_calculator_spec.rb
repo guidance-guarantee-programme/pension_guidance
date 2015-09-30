6e84f1c5e2fc40cef7cc8fd4a27f33cafeeef60e
Steven Wilkin <stevenwilkin@gmail.com>
2015-09-30 13:08:31 +0100

    Calculate the pot size at the end of each year for 15 years

--- spec/lib/leave_pot_untouched_calculator_spec.rb
+++ spec/lib/leave_pot_untouched_calculator_spec.rb
@@ -3,13 +3,21 @@ RSpec.describe LeavePotUntouchedCalculator, '#pot' do
   let(:annual_saving) { 2_000 }
   let(:duration) { 5 }
   let(:net_growth_rate) { 3 }
-  let(:final_pot) { 68_900.52 }
+  let(:pot_growth) do
+    [
+      53_560.00,
+      57_226.80,
+      61_003.60,
+      64_893.71,
+      68_900.52
+    ]
+  end
 
   subject(:pot) do
     LeavePotUntouchedCalculator.new(initial_pot, annual_saving, duration, net_growth_rate).pot
   end
 
-  it 'calculates the pot size at the end of the given time period' do
-    expect(pot).to eq(final_pot)
+  it 'calculates the pot size over the given time period' do
+    expect(pot).to eq(pot_growth)
   end
 end
