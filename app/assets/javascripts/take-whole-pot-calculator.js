795747a26ab1336cdb36bfce37c26d3f4fc2635f
Ben Barnett <ben@brushfiredesign.net>
2015-11-09 09:44:41 +0000

    Display loading status and scroll to result

--- app/assets/javascripts/take-whole-pot-calculator.js
+++ app/assets/javascripts/take-whole-pot-calculator.js
@@ -5,13 +5,32 @@
     init: function() {
       var $calculator = $('.js-take-whole-pot-calculator form');
       var $result = $('<div class="calculator__result" aria-live="polite"></div>').appendTo($calculator);
+      var $loadingStatus = $('<span class="calculator__loading-status">Please wait...</span>').
+                            insertAfter($calculator.find('#js-calculate'));
 
-      $calculator.on('submit', function(event) {
+      $calculator.on('submit', $.proxy(function(event) {
         event.preventDefault();
-        $.get($calculator.attr('action'), $calculator.serialize()).then(function(data) {
+
+        this.toggleLoading($loadingStatus, true);
+
+        $.get($calculator.attr('action'), $calculator.serialize()).then($.proxy(function(data) {
           $result.html(data).removeAttr('aria-live').attr('aria-live', 'polite');
-        });
-      });
+
+          this.scrollTo($result).then($.proxy(function() {
+            this.toggleLoading($loadingStatus, false);
+          }, this));
+        }, this));
+      }, this));
+    },
+
+    toggleLoading: function($el, to) {
+      $el[to ? 'show' : 'hide']();
+    },
+
+    scrollTo: function($el) {
+      return $('html, body').animate({
+        scrollTop: $el.offset().top
+      }, 500).promise();
     }
   };
 
