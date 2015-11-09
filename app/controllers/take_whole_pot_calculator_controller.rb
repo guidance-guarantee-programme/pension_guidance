695176e6cba81d79adddf42aa0caab5436a4a8f3
Steven Wilkin <stevenwilkin@gmail.com>
2015-11-09 15:24:21 +0000

    Use the form object and render validation errors

--- app/controllers/take_whole_pot_calculator_controller.rb
+++ app/controllers/take_whole_pot_calculator_controller.rb
@@ -1,32 +1,17 @@
 class TakeWholePotCalculatorController < ApplicationController
   layout 'guides'
 
-  before_action :set_params
-
   def show
-    result = TakeWholePotCalculator.new(@pot, total_income)
-    @pot_received = result.pot_received
-    @pot_tax = result.pot_tax
+    @pot = params[:pot]
+    @income = params[:income]
+    @pension = params[:pension]
+    @pension_frequency = params[:pension_frequency]
+
+    @calculator = TakeWholePotCalculatorForm.new(
+      pot: @pot, income: @income, pension: @pension,
+      pension_frequency: @pension_frequency)
+    @result = @calculator.result
 
     return render partial: 'results' if request.xhr?
-  end
-
-  private
-
-  def set_params
-    @pot = params[:pot].to_i
-    @income = params[:income].to_i
-    @pension = params[:pension].to_i
-    @pension_frequency = params[:pension_frequency]
-  end
-
-  def total_income
-    annual_pension = case @pension_frequency
-                     when 'weekly'
-                       @pension * 52
-                     when 'annually'
-                       @pension
-                     end
-    @income + annual_pension
   end
 end
