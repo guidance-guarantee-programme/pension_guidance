889842523a8dbed55da96cac5cf04765dda411ec
Steven Wilkin <stevenwilkin@gmail.com>
2015-11-13 17:55:34 +0000

    Ensure state pension is below the limit
    Arbitrary limit of £10,400/year currently

--- app/forms/take_whole_pot_calculator_form.rb
+++ app/forms/take_whole_pot_calculator_form.rb
@@ -3,6 +3,7 @@ require 'active_model'
 class TakeWholePotCalculatorForm
   include ActiveModel::Model
 
+  MAXIMUM_STATE_PENSION = 10_400
   WEEKS_PER_YEAR = 52
 
   attr_accessor :pot, :income, :pension, :pension_frequency
@@ -11,6 +12,7 @@ class TakeWholePotCalculatorForm
   validates :income, numericality: true
   validates :pension, numericality: true
   validates :pension_frequency, inclusion: { in: %w(weekly annually) }
+  validate :within_state_pension_limit
 
   def pot
     Float(@pot.gsub(/,/, '')) rescue @pot
@@ -30,13 +32,22 @@ class TakeWholePotCalculatorForm
 
   private
 
+  def within_state_pension_limit
+    errors.add(:pension, :above_limit) if annual_pension > MAXIMUM_STATE_PENSION
+  end
+
+  def annual_pension
+    return 0 unless pension_frequency && pension
+
+    case pension_frequency
+    when 'weekly'
+      pension * WEEKS_PER_YEAR
+    when 'annually'
+      pension
+    end
+  end
+
   def total_income
-    annual_pension = case pension_frequency
-                     when 'weekly'
-                       pension * WEEKS_PER_YEAR
-                     when 'annually'
-                       pension
-                     end
     income + annual_pension
   end
 end
