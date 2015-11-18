01bfa301c1836b2c261d5c1422dca8fed08ed8d5
Steven Wilkin <stevenwilkin@gmail.com>
2015-11-18 17:16:18 +0000

    Calculate rate for single annuity based on age

--- spec/lib/guaranteed_income_calculator_spec.rb
+++ spec/lib/guaranteed_income_calculator_spec.rb
@@ -1,7 +1,27 @@
 RSpec.describe GuaranteedIncomeCalculator do
   let(:pot) { 133_333 }
+  let(:age) { 56 }
 
-  subject(:calculator) { described_class.new(pot) }
+  subject(:calculator) { described_class.new(pot, age) }
 
   specify { expect(calculator.tax_free_lump_sum).to eq(33_333.25) }
+
+  describe '#single_annuity' do
+    scenarios =
+      {
+        55 => 4_614,
+        60 => 5_160,
+        65 => 5_828,
+        70 => 6_670,
+        75 => 7_875
+      }
+
+    scenarios.each do |age, income|
+      describe "at age #{age}" do
+        subject { described_class.new(pot, age).single_annuity.round }
+
+        it { is_expected.to eq(income) }
+      end
+    end
+  end
 end
