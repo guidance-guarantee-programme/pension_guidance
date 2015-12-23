d3190615d1f18822ca163913ec8d65e54c99afdf
Ben Barnett <ben@brushfiredesign.net>
2015-12-23 16:50:00 +0000

    Adds global footer
    Styles taken from gov.uk, but adapted and converted to BEM conventions.

---
+++ app/view_models/navigation.rb
@@ -0,0 +1,74 @@
+class Navigation
+  Item = Struct.new(:id, :label, :url)
+  Topic = Struct.new(:id, :label, :items)
+
+  attr_accessor :taxonomy
+  private :taxonomy=
+
+  def initialize(taxonomy)
+    self.taxonomy = taxonomy
+  end
+
+  def topics
+    topics = parented.collect do |node|
+      category = node.content
+      guides = find_guides(node.children).reject(&:empty?)
+
+      Topic.new(category.id, category.title, guides)
+    end
+
+    topics << more_topic unless more_topic.items.empty?
+    topics
+  end
+
+  private
+
+  def find_guides(nodes)
+    [].tap do |guides|
+      guides.push(*find_parented_guides(nodes))
+      guides.push(find_orphan_guides(nodes))
+    end
+  end
+
+  def find_orphan_guides(nodes)
+    nodes.reject(&:has_children?).collect(&:content).collect(&GuideDecorator.method(:cached_for)).collect do |guide|
+      build_item(guide)
+    end
+  end
+
+  def find_parented_guides(nodes)
+    [].tap do |guides|
+      nodes.select(&:has_children?).each do |parent|
+        guides << parent.children.collect(&:content).collect(&GuideDecorator.method(:cached_for)).collect do |guide|
+          build_item(guide)
+        end
+      end
+    end
+  end
+
+  def build_item(guide)
+    Item.new(guide.id, guide.label, guide.url)
+  end
+
+  def more_category
+    @more_category ||= CategoryRepository.new.find('more')
+  end
+
+  def more_topic
+    @more_topic ||= Topic.new(more_category.id, more_category.title, [orphan_guides])
+  end
+
+  def orphans
+    @orphans ||= taxonomy.children.reject(&:has_children?)
+  end
+
+  def parented
+    @parented ||= taxonomy.children.select(&:has_children?)
+  end
+
+  def orphan_guides
+    @orphan_guides ||= orphans.collect(&:content).collect(&GuideDecorator.method(:cached_for)).collect do |guide|
+      Item.new(guide.id, guide.label, guide.url)
+    end
+  end
+end
--- features/step_definitions/home_page_steps.rb
+++ features/step_definitions/home_page_steps.rb
@@ -6,3 +6,12 @@ end
 Then(/^I see links to guides/) do
   expect(@homepage).to have_links
 end
+
+Then(/^I can navigate to guides from the footer$/) do
+  expect(@homepage.footer).to have_categories
+
+  @homepage.footer.categories.each do |category|
+    expect(category).to have_header
+    expect(category).to have_links
+  end
+end
