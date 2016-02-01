da7051c44774cf6698145af60d6db7d505ff732d
Andrew Garner <andrew@andrewgarner.com>
2016-02-01 14:40:34 +0000

    Configure acceptance tests with javascript support

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -3,6 +3,11 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'features/cassettes'
   c.hook_into :webmock
+  c.ignore_request do |req|
+    # Don't mock the call that Poltergeist polls while waiting for
+    # Phantomjs to load (http://localhost:<random port>/__identify__)
+    req.uri =~ %r{/__identify__$}
+  end
   c.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV['GOOGLE_API_KEY'] }
   c.filter_sensitive_data('<GOOGLE_API_CX>') { ENV['GOOGLE_API_CX'] }
 end
