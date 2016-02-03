201132dcb01daa673ef3ff7427c0fd4a12a73296
Andrew Garner <andrew@andrewgarner.com>
2016-02-03 19:30:01 +0000

    Tidy up Take Whole Pot Calculator spec
    Saves some whitespace.

--- spec/lib/take_whole_pot_calculator_spec.rb
+++ spec/lib/take_whole_pot_calculator_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe TakeWholePotCalculator do
   describe '.personal_allowance_for' do
-    let(:income) { 0 }
-
     subject(:personal_allowance) { described_class.personal_allowance_for(income) }
+
+    let(:income) { 0 }
 
     context 'when £100,000 and below total income' do
       let(:income) { 100_000 }
@@ -24,58 +24,34 @@ RSpec.describe TakeWholePotCalculator do
   end
 
   describe '.marginal_tax_for_pot_with_income' do
+    subject(:tax) { described_class.marginal_tax_for_pot_with_income(pot, income) }
+
     let(:pot) { 0 }
     let(:income) { 0 }
-
-    subject(:tax) { described_class.marginal_tax_for_pot_with_income(pot, income) }
 
     context 'when income is 0' do
       describe 'and taxable portion of pot is within the personal allowance' do
         let(:pot) { 10_000 }
 
-        it do
-          is_expected.to eq(
-            basic: 0,
-            higher: 0,
-            additional: 0
-          )
-        end
+        it { is_expected.to eq(basic: 0, higher: 0, additional: 0) }
       end
 
       describe 'and taxable portion of pot is within the basic rate band' do
         let(:pot) { 20_000 }
 
-        it do
-          is_expected.to eq(
-            basic: 880,
-            higher: 0,
-            additional: 0
-          )
-        end
+        it { is_expected.to eq(basic: 880, higher: 0, additional: 0) }
       end
 
       describe 'and taxable portion of pot is within the higher rate band' do
         let(:pot) { 60_000 }
 
-        it do
-          is_expected.to eq(
-            basic: 6357,
-            higher: 1046,
-            additional: 0
-          )
-        end
+        it { is_expected.to eq(basic: 6357, higher: 1046, additional: 0) }
       end
 
       describe 'and taxable portion of pot is within the additional rate band' do
         let(:pot) { 220_000 }
 
-        it do
-          is_expected.to eq(
-            basic: 6357,
-            higher: 47_286,
-            additional: 6750
-          )
-        end
+        it { is_expected.to eq(basic: 6357, higher: 47_286, additional: 6750) }
       end
     end
 
@@ -85,37 +61,19 @@ RSpec.describe TakeWholePotCalculator do
       describe 'and taxable portion of pot is within the basic rate band' do
         let(:pot) { 20_000 }
 
-        it do
-          is_expected.to eq(
-            basic: 3000,
-            higher: 0,
-            additional: 0
-          )
-        end
+        it { is_expected.to eq(basic: 3000, higher: 0, additional: 0) }
       end
 
       describe 'and taxable portion of pot is within the higher rate band' do
         let(:pot) { 40_000 }
 
-        it do
-          is_expected.to eq(
-            basic: 5477,
-            higher: 1046,
-            additional: 0
-          )
-        end
+        it { is_expected.to eq(basic: 5477, higher: 1046, additional: 0) }
       end
 
       describe 'and taxable portion of pot is within the additional rate band' do
         let(:pot) { 200_000 }
 
-        it do
-          is_expected.to eq(
-            basic: 3357,
-            higher: 47_286,
-            additional: 6750
-          )
-        end
+        it { is_expected.to eq(basic: 3357, higher: 47_286, additional: 6750) }
       end
     end
 
@@ -125,26 +83,14 @@ RSpec.describe TakeWholePotCalculator do
       describe 'and taxable portion of pot is within the higher rate band' do
         let(:pot) { 20_000 }
 
-        it do
-          is_expected.to eq(
-            basic: 0,
-            higher: 6000,
-            additional: 0
-          )
-        end
+        it { is_expected.to eq(basic: 0, higher: 6000, additional: 0) }
       end
 
       describe 'and taxable portion of pot is within the additional rate band' do
         let(:income) { 160_000 }
         let(:pot) { 20_000 }
 
-        it do
-          is_expected.to eq(
-            basic: 0,
-            higher: 0,
-            additional: 6750
-          )
-        end
+        it { is_expected.to eq(basic: 0, higher: 0, additional: 6750) }
       end
     end
 
@@ -152,45 +98,17 @@ RSpec.describe TakeWholePotCalculator do
       let(:pot) { 20_000 }
       let(:income) { 150_000 }
 
-      it do
-        is_expected.to eq(
-          basic: 0,
-          higher: 0,
-          additional: 6750
-        )
-      end
+      it { is_expected.to eq(basic: 0, higher: 0, additional: 6750) }
     end
   end
 
-  scenarios =
-    [{
-      income:       0,
-      pot:          10_000,
-      pot_tax:      0,
-      pot_received: 10_000
-    }, {
-      income:       10_000,
-      pot:          20_000,
-      pot_tax:      2_880,
-      pot_received: 17_120
-    }, {
-      income:       30_000,
-      pot:          50_000,
-      pot_tax:      12_523,
-      pot_received: 37_477
-    }, {
-      income:       0,
-      pot:          160_000,
-      pot_tax:      41_403,
-      pot_received: 118_597
-    }, {
-      income:       70_000,
-      pot:          250_000,
-      pot_tax:      80_375,
-      pot_received: 169_625
-    }]
-
-  scenarios.each do |scenario|
+  [
+    { income: 0, pot: 10_000, pot_tax: 0, pot_received: 10_000 },
+    { income: 10_000, pot: 20_000, pot_tax: 2_880, pot_received: 17_120 },
+    { income: 30_000, pot: 50_000, pot_tax: 12_523, pot_received: 37_477 },
+    { income: 0, pot: 160_000, pot_tax: 41_403, pot_received: 118_597 },
+    { income: 70_000, pot: 250_000, pot_tax: 80_375, pot_received: 169_625 }
+  ].each do |scenario|
     context "with a pot of £#{scenario[:pot]} and an income of £#{scenario[:income]}" do
       subject(:calculator) { TakeWholePotCalculator.new(scenario[:pot], scenario[:income]) }
 
