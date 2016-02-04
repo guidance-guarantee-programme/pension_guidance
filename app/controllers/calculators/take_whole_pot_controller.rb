5e57bd26231932b4800c551f3c0ffc0762ee96b0
Andrew Garner <andrew@andrewgarner.com>
2016-02-04 12:08:28 +0000

    Update Take Whole Pot to new calculator convention

--- app/controllers/calculators/take_whole_pot_controller.rb
+++ app/controllers/calculators/take_whole_pot_controller.rb
@@ -1,14 +1,11 @@
 module Calculators
   class TakeWholePotController < CalculatorsController
     def show
-      @pot = form_params[:pot]
-      @income = form_params[:income]
-
-      @calculator = TakeWholePotForm.new(form_params)
+      @form = TakeWholePotForm.new(form_params)
 
       render partial: 'calculators/take_whole_pot/calculator',
-             locals: { calculator: @calculator, pot: @pot, income: @income },
-             status: (@calculator.invalid? ? :bad_request : :ok) if request.xhr?
+             locals: { form: @form },
+             status: (@form.invalid? ? :bad_request : :ok) if request.xhr?
     end
 
     private
