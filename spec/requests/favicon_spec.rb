a3c3f08a61bd0b1f87a249568eb608703840db46
Ben Lovell <benjamin.lovell@gmail.com>
2016-05-23 13:44:49 +0100

    Remove now redundant favicon spec
    This was throwing up various deprecation messages.

--- spec/requests/favicon_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe 'Favicon', type: :request do
-  context 'requesting a favicon.ico' do
-    it 'returns a 404 response' do
-      get '/favicon.ico'
-
-      expect(response.status).to eq(404)
-    end
-  end
-end
