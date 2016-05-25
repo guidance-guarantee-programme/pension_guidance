a9c11b08f8049fd149e2670174e6f24c64f20add
Ben Barnett <ben@brushfiredesign.net>
2016-05-25 14:17:10 +0100

    Use server generated dates and morning/afternoon times

--- app/controllers/booking_requests_controller.rb
+++ app/controllers/booking_requests_controller.rb
@@ -2,6 +2,11 @@ class BookingRequestsController < ApplicationController
   layout 'full_width'
 
   def new
+    @booking_request = BookingRequest.create
+
+    @primary_slot = @booking_request.build_primary_slot
+    @secondary_slot = @booking_request.build_secondary_slot
+    @tertiary_slot = @booking_request.build_tertiary_slot
   end
 
   def create
