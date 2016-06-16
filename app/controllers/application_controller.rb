e5eacff98a00924648778b6f9cce6cedd68b4de0
Ben Barnett <ben@brushfiredesign.net>
2016-06-16 11:33:42 +0100

    Implementing alternative homepage for A/B experiment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,13 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery
 
+  helper_method :footer?
+
   if ENV['AUTH_USERNAME'] && ENV['AUTH_PASSWORD']
     http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
+  end
+
+  def footer?
+    true
   end
 end
