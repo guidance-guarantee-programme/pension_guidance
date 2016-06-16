9128c7dfbe7bc0d9b5d1fcc38f0fc9ef2444ca64
Ben Lovell <benjamin.lovell@gmail.com>
2016-06-16 11:55:40 +0100

    WIP: Bind `Location` to deserialized API data

--- lib/booking_locations/location.rb
+++ lib/booking_locations/location.rb
@@ -1,6 +1,31 @@
 module BookingLocations
   class Location
     def initialize(data)
+      @data = data
+    end
+
+    def id
+      @data['uid']
+    end
+
+    def name
+      @data['name']
+    end
+
+    def address
+      @data['address']
+    end
+
+    def locations
+      @locations ||= @data['locations'].map { |child_data| Location.new(child_data) }
+    end
+
+    def guiders
+      @guiders ||= @data['guiders'].map { |guider| OpenStruct.new(guider) }
+    end
+
+    def slots
+      @slots ||= @data['slots'].map { |slot| OpenStruct.new(slot) }
     end
   end
 end
