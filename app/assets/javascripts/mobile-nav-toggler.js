8e1507629ea3e16007bc5d5bb61981052a201e05
Peter Guntrip <peter@guntrisoft.com>
2016-12-06 12:56:50 +0000

    Improved accessibility of mobile version of the site navigation
    - Altered menu link to be a button which better describes
      that it's going to do something interactive when clicked on
    - Added aria-expanded=true/false to read out to screen reader
      users that the item that it controls is collapsed, and reads
      out that it's expanded when clicked on
    - Added aria-hidden=true on the menu items by default, and then
      turned this to true when button is clicked on, so that menu
      items are only read out when the menu is expanded
    - Added aria-controls for screenreaders which support it, so
      a user can jump directly to the menu from the button

--- app/assets/javascripts/mobile-nav-toggler.js
+++ app/assets/javascripts/mobile-nav-toggler.js
@@ -8,7 +8,16 @@
 
       $trigger.click(function(e) {
         e.preventDefault();
-        $target.toggleClass('active');
+
+        if ($target.hasClass('active')) {
+          $target.removeClass('active');
+          $target.attr('aria-hidden', true);
+          $trigger.attr('aria-expanded', false);
+        } else {
+          $target.addClass('active');
+          $target.attr('aria-hidden', false);
+          $trigger.attr('aria-expanded', true);
+        }
       });
     }
   };
