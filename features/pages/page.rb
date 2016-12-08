ccad2e2c9b6b52287015a24b69e3f4efce90c64c
Andrew Vos <andrew.vos@gmail.com>
2016-12-08 18:46:07 +0000

    Use visible: false for finding elements that are not visible
    In the new version of poltergeist, the previous code would fail.

--- features/pages/page.rb
+++ features/pages/page.rb
@@ -8,14 +8,14 @@ module Pages
     section :footer, Sections::Footer, '#footer'
 
     def check_hidden_checkbox(element, check)
-      input = element.find('input[type=checkbox]')
+      input = element.find('input[type=checkbox]', visible: false)
       input.trigger('click') if input['checked'] != check
 
       raise 'value did not change' if input['checked'] != check
     end
 
     def check_hidden_radio(element)
-      input = element.find('input[type=radio]')
+      input = element.find('input[type=radio]', visible: false)
       input.trigger('click')
     end
   end
