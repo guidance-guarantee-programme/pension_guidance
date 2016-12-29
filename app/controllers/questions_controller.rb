b4c67f9077dedfb715dd255e46ae2af560d82077
David Henry <david@decoybecoy.com>
2016-12-29 10:05:53 +0000

    Fix redirection bug
    When redirecting to a string the following URL is used:
    
        # actionpack-4.2.7.1 lib/action_controller/metal/redirecting:89
        request.protocol + request.host_with_port + options
    
    This means `redirect_to 'pension-type-tool/question-1'` was
    redirecting to:
    
        https://staging-www.pensionwise.gov.ukpension-type-tool/question-1
    
    Not sure why this issue did not present locally.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,6 @@ class QuestionsController < ApplicationController
     question = GuideDecorator.cached_for(GuideRepository.new.find(params[:question_id]))
     answer = question.metadata.answers[params[:response]]
 
-    redirect_to answer || params[:question_id]
+    redirect_to answer || "/#{params[:question_id]}"
   end
 end
