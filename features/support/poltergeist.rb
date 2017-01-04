5f4be5e1691488e76eeb954d162baf13c7b0ba75
Ben Lovell <benjamin.lovell@gmail.com>
2017-01-04 17:32:49 +0000

    Whitelist local assets only during JS tests
    This stops our tests loading remote assets, speeding up the tests
    generally but also silencing the stupid console output from smart
    survey's scripts.

--- features/support/poltergeist.rb
+++ features/support/poltergeist.rb
@@ -1,4 +1,8 @@
 require 'capybara/poltergeist'
 
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, url_whitelist: %w(127.0.0.1))
+end
+
 Capybara.javascript_driver = :poltergeist
 Capybara.default_max_wait_time = 20
