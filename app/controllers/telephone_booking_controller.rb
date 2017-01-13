e2a89a939063f02ca4b3c879dd516d9caa9a0912
Peter Guntrip <peter@guntrisoft.com>
2017-01-13 09:33:06 +0000

    working on changing calendar to radio buttons

--- app/controllers/telephone_booking_controller.rb
+++ app/controllers/telephone_booking_controller.rb
@@ -14,34 +14,40 @@ class TelephoneBookingController < ApplicationController
   end
 
   def set_vars
-    if params[:month]
-      slot = Date.parse("#{params[:month]}-01")
-    else
-      slot = @available_days.first
+    first_slot = @available_days.first
+    last_slot = @available_days.last
+
+    first_day = first_slot.beginning_of_month
+    last_day = last_slot.end_of_month
+
+    @months = (first_day..last_day).to_a.uniq do |day|
+      day.strftime('%m')
+    end.map do |day|
+      (day.beginning_of_month..day.end_of_month).to_a
     end
 
-    beginning_of_month = slot.beginning_of_month
-    end_of_month = slot.end_of_month
+    # beginning_of_month = slot.beginning_of_month
+    # end_of_month = slot.end_of_month
 
-    @month = beginning_of_month.strftime('%B')
-    @month_url_var = beginning_of_month.strftime('%Y-%m')
-    @next_month = beginning_of_month.next_month.strftime('%B')
-    @next_month_url_var = beginning_of_month.next_month.strftime('%Y-%m')
-    @prev_month = beginning_of_month.prev_month.strftime('%B')
-    @prev_month_url_var = beginning_of_month.prev_month.strftime('%Y-%m')
+    # @month = beginning_of_month.strftime('%B')
+    # @month_url_var = beginning_of_month.strftime('%Y-%m')
+    # @next_month = beginning_of_month.next_month.strftime('%B')
+    # @next_month_url_var = beginning_of_month.next_month.strftime('%Y-%m')
+    # @prev_month = beginning_of_month.prev_month.strftime('%B')
+    # @prev_month_url_var = beginning_of_month.prev_month.strftime('%Y-%m')
 
-    @is_first_month = slot.strftime('%Y-%m') == @available_days.first.strftime('%Y-%m')
-    @is_last_month = slot.strftime('%Y-%m') == @available_days.last.strftime('%Y-%m')
+    # @is_first_month = slot.strftime('%Y-%m') == @available_days.first.strftime('%Y-%m')
+    # @is_last_month = slot.strftime('%Y-%m') == @available_days.last.strftime('%Y-%m')
 
-    @days = (beginning_of_month..end_of_month)
+    # @days = (beginning_of_month..end_of_month)
 
-    @day_of_the_week = beginning_of_month.strftime('%u').to_i
+    # @day_of_the_week = beginning_of_month.strftime('%u').to_i
 
-    if params[:date]
-      @selected_date = Date.parse(params[:date])
-      @selected_date_formatted = @selected_date.strftime('%Y-%m-%d')
-      @available_times = @availability[@selected_date.strftime('%Y-%m-%d')]
-    end
+    # if params[:date]
+    #   @selected_date = Date.parse(params[:date])
+    #   @selected_date_formatted = @selected_date.strftime('%Y-%m-%d')
+    #   @available_times = @availability[@selected_date.strftime('%Y-%m-%d')]
+    # end
   end
 
   def set_breadcrumbs
