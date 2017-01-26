9a0e768da59c6569848b86ce46671ae712a3f82c
David Henry <david@decoybecoy.com>
2017-01-26 14:50:00 +0000

    Allow feedback to be left on the pension type tool
    This was failing to work as the cookies are striped
    from the page to enable caching. This change requires
    CSRF protection from the feedback submission form.

---
+++ spec/features/feedback_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+RSpec.feature 'Customer feedback', js: true do
+  scenario 'can be left on the pension type tool' do
+    begin
+      allow_forgery_protection = FeedbacksController.allow_forgery_protection
+      FeedbacksController.allow_forgery_protection = true
+
+      visit '/pension-type-tool'
+
+      click_on 'Is there anything wrong with this page?'
+
+      fill_in 'Name', with: 'Jim Bob'
+      fill_in 'Email', with: 'jim@bob.com'
+      fill_in 'Message', with: 'Some feedback'
+
+      click_on 'Send feedback'
+
+      expect(page).to have_content('Thank you for your help')
+    ensure
+      FeedbacksController.allow_forgery_protection = allow_forgery_protection
+    end
+  end
+end
