590cc8578a07a7a10049cbf31c6e29dfb17bfb79
Peter Guntrip <peter@guntrisoft.com>
2017-03-30 14:41:56 +0100

    Update govuk_elements/govuk_template/govuk_frontend_toolkit
    This updates:
    
    - govuk elements to version 3.0.2
    - govuk frontend toolkit to version 5.2.0
    - govuk template to version 0.20.0
    
    On upgrading to 3.0.2 of elements I've had to rework the markup for
    radio/checkboxes. They no longer require javascript to update
    the UI.
    
    Another visible change is the use of text-decoration-skip: ink;
    which changes the appearance of underlines for links across
    the site on Chrome.
    
    Full release notes for these changes are here:
    
    https://github.com/alphagov/govuk_elements/releases
    https://github.com/alphagov/govuk_template/blob/master/CHANGELOG.md
    https://github.com/alphagov/govuk_frontend_toolkit/releases

--- features/pages/page.rb
+++ features/pages/page.rb
@@ -6,17 +6,5 @@ module Pages
     elements :link_promo_items, '.t-link-promo__item'
 
     section :footer, Sections::Footer, '#footer'
-
-    def check_hidden_checkbox(element, check)
-      input = element.find('input[type=checkbox]', visible: false)
-      input.trigger('click') if input['checked'] != check
-
-      raise 'value did not change' if input['checked'] != check
-    end
-
-    def check_hidden_radio(element)
-      input = element.find('input[type=radio]', visible: false)
-      input.trigger('click')
-    end
   end
 end
