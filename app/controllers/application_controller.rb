35132738ae607640e4a44eea51351290de5ba234
Peter Guntrip <peter@guntrisoft.com>
2017-04-07 16:08:12 +0100

    Adding language banner to pages which don't have a welsh translation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
-  helper_method :footer?, :alternate_locales
+  helper_method :footer?, :alternate_locales, :show_language_banner?
 
   if ENV['AUTH_USERNAME'] && ENV['AUTH_PASSWORD']
     http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
@@ -30,5 +30,9 @@ class ApplicationController < ActionController::Base
 
   def default_url_options(options = {})
     options.reverse_merge(locale: I18n.locale)
+  end
+
+  def show_language_banner?
+    I18n.locale != I18n.default_locale
   end
 end
