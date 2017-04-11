161f22b4cd345e8b9c56304e6e894f8064da14e6
Jon Gilbraith <jon@hobocode.io>
2017-04-11 16:45:04 +0100

    Account for pages in welsh locale that are in english

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -4,6 +4,21 @@ class GuidesController < ApplicationController
   before_action :find_guide
   before_action :set_breadcrumbs
 
+  UNTRANSLATED_GUIDE_SLUGS = %w(
+    appointments
+    book-face-to-face
+    book-phone
+    pension-type-tool
+    pension-type-tool/defined-contribution
+    pension-type-tool/might-defined-contribution
+    pension-type-tool/most-cases-defined-benefit
+    pension-type-tool/most-cases-defined-contribution
+    pension-type-tool/question-1
+    pension-type-tool/question-2
+    pension-type-tool/question-3
+    pension-type-tool/question-4
+  ).freeze
+
   def show
     expires_in Rails.application.config.cache_max_age, public: true
   end
@@ -23,7 +38,7 @@ class GuidesController < ApplicationController
     breadcrumb(Breadcrumb.pension_options) if @guide.option?
   end
 
-  def show_language_banner?
-    false
+  def content_lang_matches_locale?
+    UNTRANSLATED_GUIDE_SLUGS.exclude?(params[:id])
   end
 end
