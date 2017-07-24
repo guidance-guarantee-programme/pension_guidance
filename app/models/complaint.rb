b67c0b25c212a1b3e4ddecae704953b3fd58459d
Jon Gilbraith <jon@hobocode.io>
2017-07-24 15:24:47 +0100

    Add formalised complaints form
    This lets us collect more information to collate complaints with
    bookings that happen in both channels.

---
+++ app/models/complaint.rb
@@ -0,0 +1,53 @@
+class Complaint
+  include ActiveModel::Model
+
+  attr_accessor :name, :email_address
+  attr_accessor :phone_booking_message, :face_to_face_message, :other_message
+
+  validates :name, presence: true
+  validates :email_address, email: true, allow_blank: true
+
+  def send_to_zendesk
+    ZenDesk.create_ticket(zendesk_content) if valid?
+
+    valid?
+  end
+
+  private
+
+  def zendesk_content
+    {
+      subject: 'Complaint',
+      name: name,
+      email: email_address,
+      message: combined_message,
+      tags: %w(complaint)
+    }
+  end
+
+  def combined_message # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    @combined_message ||= begin
+      @combined_message = ''
+
+      if phone_booking_message.present?
+        @combined_message << "About a Phone Booking\n"
+        @combined_message << "---------------------\n"
+        @combined_message << "#{phone_booking_message}\n\n"
+      end
+
+      if face_to_face_message.present?
+        @combined_message << "About a Face to Face Booking\n"
+        @combined_message << "---------------------\n"
+        @combined_message << "#{face_to_face_message}\n\n"
+      end
+
+      if other_message.present?
+        @combined_message << "About something else\n"
+        @combined_message << "---------------------\n"
+        @combined_message << other_message
+      end
+
+      @combined_message
+    end
+  end
+end
