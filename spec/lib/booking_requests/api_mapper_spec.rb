67bf05ed12c31d2c72bb2ed9f1dab92e12500a10
Ben Lovell <benjamin.lovell@gmail.com>
2017-08-22 12:31:44 +0100

    Identify bookings placed by a TP agent
    This identifies bookings based on the agent's remote IP address.

--- spec/lib/booking_requests/api_mapper_spec.rb
+++ spec/lib/booking_requests/api_mapper_spec.rb
@@ -18,7 +18,8 @@ RSpec.describe BookingRequests::ApiMapper do
       accessibility_requirements: false,
       additional_info: nil,
       opt_in: false,
-      dc_pot: 'yes'
+      dc_pot: 'yes',
+      remote_ip: '127.0.0.1'
     )
   end
 
@@ -40,6 +41,7 @@ RSpec.describe BookingRequests::ApiMapper do
           additional_info: '',
           marketing_opt_in: false,
           defined_contribution_pot_confirmed: true,
+          placed_by_agent: false,
           slots: [
             { priority: 1, date: '2016-01-01', from: '0900', to: '1300' },
             { priority: 2, date: '2016-01-01', from: '1300', to: '1700' }
