b2e89cff1828049dc7781b31748fdb15e90788dc
Ben Lovell <benjamin.lovell@gmail.com>
2021-03-19 09:53:41 +0000

    Update annuity rates

--- app/lib/guaranteed_income_calculator.rb
+++ app/lib/guaranteed_income_calculator.rb
@@ -34,11 +34,11 @@ class GuaranteedIncomeCalculator
   # https://docs.google.com/spreadsheets/d/18oScEdlwr-msjdhFIotqpi4Z_6UnKR5xiQO5ctmEnRE/edit#gid=0
   def annuity_rate
     case age
-    when 55...60 then 0.03642
-    when 60...65 then 0.04075
-    when 65...70 then 0.04852
-    when 70...75 then 0.05691
-    else              0.06850
+    when 55...60 then 0.03789
+    when 60...65 then 0.04191
+    when 65...70 then 0.04856
+    when 70...75 then 0.05696
+    else              0.07058
     end
   end
 end
