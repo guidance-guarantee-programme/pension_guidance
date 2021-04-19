af8f8701415729486d8c8527a4a60bde627199c7
Ben Lovell <benjamin.lovell@gmail.com>
2021-04-19 10:23:23 +0100

    Correct external links for Welsh bookings

--- app/controllers/welsh_language/booking_requests_controller.rb
+++ app/controllers/welsh_language/booking_requests_controller.rb
@@ -2,6 +2,8 @@ module WelshLanguage
   class BookingRequestsController < ApplicationController
     include Embeddable
 
+    helper MoneyHelper
+
     def new
       @booking_request = BookingRequest.new(booking_request_params)
     end
