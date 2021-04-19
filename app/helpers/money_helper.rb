2d205bc4c6a27a3f80003bb120795e06a09dfadd
Ben Lovell <benjamin.lovell@gmail.com>
2021-04-19 10:12:47 +0100

    Address external links for explore your options
    External links cannot open in iframes and links to now editable content
    should link back to the AEM hosted pages instead.

---
+++ app/helpers/money_helper.rb
@@ -0,0 +1,11 @@
+module MoneyHelper
+  def money_helper_url(path, locale: :en)
+    prefix = ENV.fetch('MONEY_HELPER_CONTENT_PREFIX') { 'https://moneypensions-stage.adobemsbasic.com' }
+
+    "#{prefix}/#{locale}/#{path}"
+  end
+
+  def blah
+    'hi'
+  end
+end
