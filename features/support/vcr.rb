b674da73422800ba1c5832f540ff8989d8d3330f
Ben Lovell <benjamin.lovell@gmail.com>
2021-11-03 16:36:17 +0000

    Ensure cucumber can download chromedriver

--- features/support/vcr.rb
+++ features/support/vcr.rb
@@ -6,6 +6,8 @@ VCR.configure do |c|
   c.ignore_localhost = true
   c.filter_sensitive_data('<GOOGLE_API_KEY>') { ENV['GOOGLE_API_KEY'] }
   c.filter_sensitive_data('<GOOGLE_API_CX>') { ENV['GOOGLE_API_CX'] }
+
+  c.ignore_hosts 'chromedriver.storage.googleapis.com'
 end
 
 VCR.cucumber_tags do |t|
