130fa4d590319d64eb9e8ebfdd022adda6690d94
Andrew Vos <andrew.vos@gmail.com>
2022-01-13 15:53:23 +0000

    Don't sanitize HTML in Govspeak markdown
    Newer versions of Govspeak do this by default: https://github.com/alphagov/govspeak/pull/144

--- app/decorators/govspeak_guide_decorator.rb
+++ app/decorators/govspeak_guide_decorator.rb
@@ -1,6 +1,6 @@
 # rubocop:disable Rails/OutputSafety
 class GovspeakGuideDecorator < GuideDecorator
   def content
-    @content ||= Govspeak::Document.new(guide.content).to_html.html_safe
+    @content ||= Govspeak::Document.new(guide.content, sanitize: false).to_html.html_safe
   end
 end
