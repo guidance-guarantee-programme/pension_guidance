ffa7ae5efd9e6df43d50420bd45126d4846c999e
Ben Lovell <benjamin.lovell@gmail.com>
2022-05-12 15:49:32 +0100

    Display help text for embedded nudge
    Customises the help text when the appointment is nudged and embedded.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,24 @@
 module ApplicationHelper
   include NavigationHelper
 
+  def contact_number(appointment)
+    if appointment.due_diligence?
+      '0800 015 4906'
+    elsif appointment.embedded?
+      '0800 100 166'
+    else
+      '0800 138 3944'
+    end
+  end
+
+  def contact_hours(appointment)
+    if appointment.due_diligence?
+      '9am to 5pm'
+    else
+      '8am to 8pm'
+    end
+  end
+
   def book_an_appointment_link?
     true
   end
