b298dd49589346c887357bd4e4e403b4652de29c
Ben Lovell <benjamin.lovell@gmail.com>
2022-06-16 22:39:05 +0100

    Ensure PSG customers remain in-channel
    When opting to change a date and time the customers would end up
    out-of-channel and book a regular telephone appointment instead. This
    change ensures the correct flag is sent.

--- features/pages/new_telephone_appointment.rb
+++ features/pages/new_telephone_appointment.rb
@@ -18,6 +18,7 @@ module Pages
     element :smarter_signposting_banner, '.t-smarter-signposted-banner'
     element :cancel_smarter_signposting, '.t-cancel-smarter-signposting'
     element :lloyds_signposting_banner, '.t-lloyds-signposted-banner'
+    element :change_date_time, '.t-change-date-time'
 
     element :dc_pot_confirmed_yes, '.t-dc-pot-confirmed-yes'
     element :dc_pot_confirmed_no, '.t-dc-pot-confirmed-no'
