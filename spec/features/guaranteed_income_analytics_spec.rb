01fc2f0a44e94cd9bbe67d51c8c100c30ea9f8e7
Ben Lovell <benjamin.lovell@gmail.com>
2023-01-13 13:32:32 +0000

    Update annuity rates

--- spec/features/guaranteed_income_analytics_spec.rb
+++ spec/features/guaranteed_income_analytics_spec.rb
@@ -19,7 +19,7 @@ RSpec.feature 'Guaranteed income analytics', type: :feature, js: true do
 
     expect(data_layer).to include(
       'event'      => 'CalculatorEstimate',
-      'pot_payout' => 3800,
+      'pot_payout' => 4600,
       'age'        => 60,
       'pot'        => 100_000,
       'valid'      => true
