f86383407cb28c4d5ac5d8c68b9ec2401502b202
Ben Lovell <benjamin.lovell@gmail.com>
2023-04-29 16:11:27 +0100

    Configure redis SSL params

--- config/initializers/redis_pool.rb
+++ config/initializers/redis_pool.rb
@@ -1,4 +1,4 @@
 REDIS_POOL = ConnectionPool.new(
   size:    ENV.fetch('REDIS_POOL_SIZE') { 10 }.to_i,
   timeout: ENV.fetch('REDIS_POOL_TIMEOUT') { 5 }.to_i
-) { Redis.new }
+) { Redis.new(ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }) }
