9b92e4fecfed34f33997361f5a91e0338f83888a
emilysatodossantos <emily.satodossantos@solirius.com>
2023-08-11 11:31:49 +0100

    pin chromedriver

--- features/support/chrome.rb
+++ features/support/chrome.rb
@@ -10,6 +10,9 @@ Capybara.register_driver :chrome_headless do |app|
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
+Webdrivers::Chromedriver.required_version = '114.0.5735.90'
+Webdrivers.install_dir = '/opt/homebrew/bin' unless ENV['TRAVIS']
+
 Capybara.default_normalize_ws = true
 Capybara.default_max_wait_time = 10 if ENV['TRAVIS']
 Capybara.server = :puma, { Silent: true }
