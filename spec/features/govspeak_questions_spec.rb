dbd8584e34360fb577e7dc9c9db3e2589e2786af
emilysatodossantos <emily.satodossantos@solirius.com>
2023-12-15 11:30:43 +0000

    Expanded path arguments

--- spec/features/govspeak_questions_spec.rb
+++ spec/features/govspeak_questions_spec.rb
@@ -1,7 +1,7 @@
 RSpec.feature 'Govspeak questions are rendered correctly' do
   scenario "it correctly renders the yes, no, don't know question" do
     id = 'yes_no_dont_know_question'
-    guide_repository = GuideRepository.new(:en, File.expand_path('../../fixtures', __FILE__))
+    guide_repository = GuideRepository.new(:en, File.expand_path('../fixtures', __dir__))
     descorated_guide = GuideDecorator.for(guide_repository.find(id))
 
     rendered_content = descorated_guide.content
