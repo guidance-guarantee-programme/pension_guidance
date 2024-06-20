0df0cfe65d2c5a03160457e27ae790b92f2977d2
Ben Lovell <benjamin.lovell@gmail.com>
2024-06-20 11:55:33 +0100

    Add customer cancellation journey
    This journey in concert with a TAP API allows the customer to cancel an
    appointment by self-serve. The cancelled appointment is processed in TAP
    accordingly and rebooks issued after are linked to the original
    cancellation for reporting purposes.

---
+++ app/controllers/cancels_controller.rb
@@ -0,0 +1,41 @@
+class CancelsController < ApplicationController
+  include Embeddable
+
+  before_action :cancellation, only: %i[new create]
+
+  def new
+  end
+
+  def create
+    if @cancellation.valid?
+      if TelephoneAppointmentsApi.new.cancel(@cancellation.to_attributes)
+        redirect_to success_cancel_path(rebooked_from: @cancellation.reference)
+      else
+        redirect_to failure_cancel_path
+      end
+    else
+      render :new
+    end
+  end
+
+  def success
+    @rebooked_from_id = params[:rebooked_from]
+  end
+
+  private
+
+  def cancellation
+    @cancellation ||= TelephoneCancellation.new(cancellation_params)
+  end
+
+  def cancellation_params
+    params
+      .fetch(:telephone_cancellation, {})
+      .permit(
+        :reference,
+        :date_of_birth_year,
+        :date_of_birth_month,
+        :date_of_birth_day
+      )
+  end
+end
---
+++ features/pages/telephone_appointment_cancellation.rb
@@ -0,0 +1,12 @@
+module Pages
+  class TelephoneAppointmentCancellation < SitePrism::Page
+    set_url '/en/telephone-appointments/cancel/new'
+
+    element :errors, '.t-errors'
+    element :booking_reference, '.t-booking-reference'
+    element :date_of_birth_day, '.t-date-of-birth-day'
+    element :date_of_birth_month, '.t-date-of-birth-month'
+    element :date_of_birth_year, '.t-date-of-birth-year'
+    element :submit, '.t-submit'
+  end
+end
