51eec20468e44c27760894dacc48e7618da3b806
Ben Lovell <benjamin.lovell@gmail.com>
2025-01-13 10:23:54 +0000

    Remove legacy zendesk contact stuff
    This is no longer used.

--- app/lib/zen_desk.rb
+++
@@ -1,7 +0,0 @@
-module ZenDesk
-  mattr_accessor :api
-
-  def self.create_ticket(**message_options)
-    api.create_ticket(**message_options)
-  end
-end
--- config/initializers/zen_desk.rb
+++
@@ -1,5 +0,0 @@
-ZenDesk.api = if Rails.env.development? || Rails.env.test?
-                ZenDesk::StubClient.new
-              else
-                ZenDesk::Client.new
-              end
