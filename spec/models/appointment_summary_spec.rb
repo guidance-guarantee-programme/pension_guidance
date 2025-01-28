857f67541809ab65436735d1b24c0634e18f0c1c
Ben Lovell <benjamin.lovell@gmail.com>
2025-01-28 15:02:27 +0000

    Provide URN to summary document when given (#1532)
    * Provide URN to summary document when given
    
    The Pension Wise Digital journey can provide the
    `appointment_summary[urn]` value and this will be presented in the
    downloaded output document PDF.
    
    * Ensure the URN is validated correctly
    
    * Allow Welsh language URN summaries to be created
    
    When the `urn` parameter is passed along to the Welsh generation action,
    this is validated and when acceptable is used to generate the customised
    download PDF.
    
    * Bump templates to released

---
+++ spec/models/appointment_summary_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe AppointmentSummary, type: :model do
+  describe 'validation' do
+    subject { described_class.new(appointment_type: 'standard', urn: 'PAB1-2ABC') }
+
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    describe '#urn' do
+      it 'permits empty or nil values' do
+        subject.urn = nil
+        expect(subject).to be_valid
+
+        subject.urn = ''
+        expect(subject).to be_valid
+      end
+
+      it 'requires a valid form' do
+        subject.urn = 'This would really suck if it were to get through!'
+        expect(subject).to be_invalid
+
+        subject.urn = 'PMY9-0GCUwhoopsie'
+        expect(subject).to be_invalid
+      end
+    end
+  end
+end
