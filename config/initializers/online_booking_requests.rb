f1da86ffb0ec1990835c1ace47b96cf3f1f3ac77
Ben Lovell <benjamin.lovell@gmail.com>
2025-03-29 09:58:21 +0000

    Explicitly require the API class

--- config/initializers/online_booking_requests.rb
+++ config/initializers/online_booking_requests.rb
@@ -4,5 +4,6 @@ BookingRequests.api = if (Rails.env.development? || Rails.env.test?) && !ENV['BO
                         require_relative '../../app/lib/booking_requests/stub_api'
                         BookingRequests::StubApi.new
                       else
+                        require_relative '../../app/lib/booking_requests/api'
                         BookingRequests::Api.new
                       end
